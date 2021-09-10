cbf6911d4e763678425ee352b1686dac068bbcf8
Gustavo Mendes <gusttavo.me@outlook.com>
2021-09-10 12:35:10 -0300

    miatoll: Update kernel path

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,7 +82,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 #TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CONFIG := cust_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6250
+TARGET_KERNEL_SOURCE := kernel/xiaomi/miatoll
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
