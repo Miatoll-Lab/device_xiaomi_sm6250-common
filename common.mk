d02a3e94c51f8a98d887ef006166990fed216444
Gustavo Mendes <gusttavo.me@outlook.com>
2021-11-20 18:41:29 -0300

    sm6250-common: Add packages to mask notch

--- common.mk
+++ common.mk
@@ -277,6 +277,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# Notch Kill
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay \
+    NotchBarKiller
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
